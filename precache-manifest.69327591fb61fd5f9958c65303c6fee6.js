dfb391c2298e1c71f2eacf6d0b6de806d360f3d0
Safa Yasin Mujdeci <mujdecisy@gmail.com>
2019-11-13 12:10:11 +0300

    1209

---
+++ precache-manifest.69327591fb61fd5f9958c65303c6fee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c3b593ccf08899484943cf347738e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2584573b002fef4c103",
+    "url": "/static/css/main.6bc5539f.chunk.css"
+  },
+  {
+    "revision": "d643b4a7d15a653c0e31",
+    "url": "/static/js/2.3b2ff184.chunk.js"
+  },
+  {
+    "revision": "c2584573b002fef4c103",
+    "url": "/static/js/main.99249978.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  }
+]);
